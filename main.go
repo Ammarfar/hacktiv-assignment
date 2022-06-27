a4a81ced7c7c771317f3c04f2be7c7c100d68500
Ammarfar <ammarfarghani@gmail.com>
2022-06-27 12:24:33 +0700

    merging assignment 1 & 2

--- main.go
+++ main.go
@@ -4,12 +4,19 @@ import (
 	"assignment/configs"
 	"assignment/controllers"
 	"assignment/helpers"
+	"fmt"
 	"net/http"
+	"os"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+
+	// Assignment 1
+	Biodata()
+
 	r := gin.Default()
 
 	configs.ConnectDB()
@@ -21,6 +28,7 @@ func main() {
 		c.JSON(http.StatusOK, helpers.ResponseSuccess("pong", nil))
 	})
 
+	// Assignment 2
 	r.POST("/orders", controllers.CreateOrder)
 	r.GET("/orders", controllers.GetOrder)
 	r.PUT("/orders/:orderId", controllers.UpdateOrder)
@@ -29,4 +37,15 @@ func main() {
 	host := helpers.GetEnv("APP_HOST")
 	port := helpers.GetEnv("APP_PORT")
 	r.Run(host + ":" + port)
+}
+
+func Biodata() {
+	input, _ := strconv.Atoi(os.Args[1])
+	user := helpers.GenerateUser(5)
+
+	if input > len(user) {
+		panic("User Not Found")
+	}
+
+	fmt.Printf("%+v\n", user[input-1])
 }
